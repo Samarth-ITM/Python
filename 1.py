30b1a66792eab88dba6015fc797fd2b13e2e9efc
RougeParrot <rougeparrot@gmail.com>
2025-11-26 15:02:31 +0530

    set

--- 1.py
+++ 1.py
@@ -1,3 +1,22 @@
-print("Hello world")
-print("hello")
-print("Hey there")
+s1 = {"apple", "banana", "cherry", "apple"}
+print(s1)
+
+s1.add("mango")
+print(s1)
+
+s2 = {"hi", "I", "am"}
+
+s1.update(s2)
+print(s1)
+
+s1.remove("mango")
+print(s1)
+
+s1.pop()
+print(s1)
+
+s1.discard("hi")
+print(s1)
+
+s1.clear()
+print(s1)
