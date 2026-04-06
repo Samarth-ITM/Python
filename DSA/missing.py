03775ba93107f322c92ef0e54804232aafc6bc23
Samarth <2025.samarths@isu.ac.in>
2026-04-06 12:14:58 +0530

    n:

--- DSA/missing.py
+++ DSA/missing.py
@@ -1,11 +1,11 @@
 nums = [2, 0]
 
 
-nums = sorted(nums)
+nums = sorted(nums) #n(logn)
 
-for i in range(len(nums)):
-    if (i+1<len(nums)):
-        if ((nums[i+1]) != nums[i]+1):
-            print(nums[i]+1)
+for i in range(len(nums)): #n 
+    if (i+1<len(nums)): #1 + 1
+        if ((nums[i+1]) != nums[i]+1): #3
+            print(nums[i]+1) #1
     else:
         continue
