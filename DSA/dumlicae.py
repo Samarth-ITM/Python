56a24b44231913a81e528d9b479df209c9d38c6b
Samarth <2025.samarths@isu.ac.in>
2026-04-06 12:14:32 +0530

    tme

--- DSA/dumlicae.py
+++ DSA/dumlicae.py
@@ -6,3 +6,5 @@ nums = [1,2,3,3]
 for i in range(len(nums)):
     if nums[i] in (nums[0:i] or nums[i+1:]):
         print(nums[i])
+        
+        
--- DSA/happy.py
+++ DSA/happy.py
@@ -1,20 +1,24 @@
 n = 19
 
-n = (list(map(int, str(abs(n)) if n < 0 else str(n))))
+n = (list(map(int, str(abs(n)) if n < 0 else str(n))))  #n
 print(n)
 
-if len(n) == 1 and n[0] == 1:
+if len(n) == 1 and n[0] == 1: #1 #1 #
     print(True)
-
+#4
 t = 0
+# n + 4
 
-while True:
-    for i in n:
-        t += i**2
-    n = t
-    n = (list(map(int, str(abs(n)) if n < 0 else str(n))))
+while True: #n
+    for i in n: #n 
+        t += i**2 #1
+        
+        
+    n = t #1
+    n = (list(map(int, str(abs(n)) if n < 0 else str(n)))) #n
     
-    if t==1:
-        return True
+    if t==1: #1
+        print(True) #1
+        break #1
     t = 0
 
